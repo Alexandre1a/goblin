160ba2ed6fd9327e1b14b30cc610d65a25908dc5
Alexandre1a <Mateialex@outlook.fr>
2025-07-19 16:01:40 +0200

    Rework commit

---
+++ internal/models/constants.go
@@ -0,0 +1,13 @@
+package consts
+
+
+// All constants used in the CLI
+const (
+	GoblinBaseDir			= ".goblin"
+	BinDir						= "bin"
+	ManifestName			= "sources.yaml"
+	LockFilePath			= "goblin.lock"
+	RawManifestURL		= "https://raw.githubusercontent/com/Alexandre1a/goblin-remote/refs/heads/main/sources.yaml"
+	CurrentOS					= runtime.GOOS
+	CurrentArch				= runtime.GOARCH
+)
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 Alexandre Delcamp--Enache <adelcampenache@gmail.com>
+This file is part of the license
+*/
+package main
+
+import "github.com/alexandre1a/goblin/cmd"
+
+func main() {
+	cmd.Execute()
+}
